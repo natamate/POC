1bd911a0eed9edd0d29ca0f78458ad3c22d156dd
natamate <naciaa.nm@gmail.com>
2017-10-22 16:49:54 +0200

    v0.99

--- lab2_2.m
+++ lab2_2.m
@@ -3,7 +3,7 @@ close all;
 clc;
 
 Image = imread('parrot.bmp');
-
+%ImageGray = rgb2gray(Image); 
 figure(1);
 imshow(Image);
 
@@ -19,36 +19,51 @@ newXX = floor(XX * xReScale);
 
 newImage = uint8(zeros(newYY, newXX));
 
-xStep = XX/newXX;
-yStep = YY/newYY;
+xStep = round(XX/newXX);
+yStep = round(YY/newYY);
 
-for ii=0:(newXX-1)
-    for jj=0:(newYY-1)
+pomImage = im2double(Image);
+
+for ii=1:(newXX-2)
+    for jj=1:(newYY-2)
         i1 = floor(ii*xStep);
         j1 = floor(jj*yStep);
+        
+        if (i1 > XX - 1)
+            i1 = XX-1;
+        end
+        
+         if (j1 > YY - 1)
+            j1 = YY-1;
+         end
+        
         A = [j1,i1];
-        B = [j1, i1+1];
-        C = [j1+1, i1+1];
-        D = [j1+1, i1];
-        
+        B = A + [0, 1];
+        C = A + [1, 1];
+        D = A + [1, 0];
+             
         i = ii/i1;
+      %{ 
         if (i > XX-1)
-            i = XX-1;
+           i = XX-1;
         end
-        
+       %} 
         j = jj/j1;    
+        %{
         if ( j > YY-1)
-            j = YY -1;
+           j = YY -1;
         end
-        
-        vA = Image(A);
-        vB = Image(B);
-        vC = Image(C);
-        vD = Image(D);
-        
-       % newImage(jj+1,ii+1) = 
+%}            
+        vA = pomImage(A(1), A(2));
+        vB = pomImage(B(1), B(2));
+        vC = pomImage(C(1), C(2));
+        vD = pomImage(D(1), D(2));
+  
+        newImage(ii+1,jj+1) = [1 - i, i] * [vA, vD; vB, vC]*[1-j;j]; 
+
     end
 end
 
 figure(2);
-imshow(newImage);
+imshow(uint8(newImage));
+%imshow(newImage);
