ec2cb7004dfa90ffe255d77375cfbeb5a7b74177
natamate <naciaa.nm@gmail.com>
2017-12-10 19:24:43 +0100

    Update bilateral_local.m

--- bilateral_local.m
+++ bilateral_local.m
@@ -14,7 +14,8 @@ for i=1:Nx
     Ncy = ceil(local_window(1)/2);
     Ncx = ceil(local_window(2)/2);
     dist = sqrt((Ncy - rows).^2 + (Ncx - cols).^2);
-    diff = reshape(abs(data(:,i) - data(((Ncy-1)*local_window(1))+Ncx, i)), local_window);    
+  %  diff = reshape(abs(data(:,i) - data(((Ncy-1)*local_window(1))+Ncx, i)), local_window);    
+    diff = reshape(abs(data(:,i) - patch(Ncy, Ncx), local_window);    
     gamma = exp(-((diff.^2)./(2*(sigmaR^2)))) .* dist; 
     
     data_filtered(i) = sum(sum(patch .* gamma))/sum(sum(gamma));
