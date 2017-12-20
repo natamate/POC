06750cac080b24e188163065b7f642b565685422
natamate <naciaa.nm@gmail.com>
2017-12-20 18:44:27 +0000

    lab10

--- Split.m
+++ Split.m
@@ -1,13 +1,12 @@
-function Split( Img,ys,ye,xs,xe )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function Split( Image, ys, ye, xs, xe)
 
-global minpodzial;
+global minpodzial; 
 global progodchylenie;
 global index;
 global segRes;
 global Mres;
-okno = Img(ys:ye,xs:xe);
+
+okno = Image(ys:ye,xs:xe);
 srednia = mean(okno(:));
 odchylenie = std(okno(:));
 bok1 = ye-ys;
@@ -15,10 +14,10 @@ bok2 = xe-xs;
 if (odchylenie > progodchylenie) && (bok1 > minpodzial) && (bok2 > minpodzial)
     podzialy = floor((ys + ye)/2);
     podzialx = floor((xs + xe)/2);
-    Split(Img,ys,podzialy,xs,podzialx);
-    Split(Img,ys,podzialy,podzialx+1,xe);
-    Split(Img,podzialy+1,ye,xs,podzialx);
-    Split(Img,podzialy+1,ye,podzialx+1,xe);
+    Split(Image,ys,podzialy,xs,podzialx);
+    Split(Image,ys,podzialy,podzialx+1,xe);
+    Split(Image,podzialy+1,ye,xs,podzialx);
+    Split(Image,podzialy+1,ye,podzialx+1,xe);
 else
     segRes(ys:ye,xs:xe) = index;
     index = index + 1;
